c9d0843bf98fb5fe177bfde973553cd9209bb5a2
Eric Lujan <eric@lujan.io>
2026-09-10 23:13:13 -0700

    Set up basic GPT architecture

--- src/gpt2/models/gpt2_model.py
+++ src/gpt2/models/gpt2_model.py
@@ -1,4 +1,7 @@
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from gpt2.model import GPT2Tokenizer, TransformerDecoder
 
 
 class GPT2Model(nn.Module):
@@ -16,14 +19,56 @@ class GPT2Model(nn.Module):
     # Dropout rate was 0.1, applied to residuals embedding, and attention.
     #
     # Instead of sinusoidal position embeddings, uses learned position embeddings.
-    #
-    # TODO:
-    # - Need to factor out the decoder block from the Transformer class
-    def __init__(self):
+    def __init__(
+        self,
+        d_model: int,
+        d_ff: int,
+        n_heads: int,
+        n_blocks: int,
+        max_context_length: int,
+        p_dropout: float,
+    ) -> None:
         super().__init__()
 
-    def forward():
-        pass
+        self.tokenizer = GPT2Tokenizer()
 
-    def reset_parameters():
-        pass
+        self.d_model = d_model
+        self.d_ff = d_ff
+        self.n_heads = n_heads
+        self.n_blocks = n_blocks
+        self.max_context_length = max_context_length
+        self.p_dropout = p_dropout
+
+        # embedding matrix
+        self.w_emb = nn.Parameter(
+            torch.empty(self.tokenizer.vocab_size(), self.d_model)
+        )
+        # positional encoding weights
+        self.w_pos = nn.Parameter(torch.empty(self.d_model, self.max_context_length))
+
+        self.decoder_blocks = nn.ParameterList(
+            [
+                TransformerDecoder(
+                    self.n_heads, self.d_model, self.d_ff, self.p_dropout
+                )
+                for _ in range(self.n_blocks)
+            ]
+        )
+
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        # Initialize embedding and position encoding weights with normal distribution
+        nn.init.normal_(self.w_emb, std=0.02)
+        nn.init.normal_(self.w_pos, std=0.02)
+
+    def forward(self, x: torch.Tensor):
+        # We don't pad in the decoder-only transformer, so we can set the decoder padding mask to all ones
+        # TODO: We can make this optional in the base class too
+        pad_mask = torch.ones_like(x, dtype=torch.bool)
+        h_0 = torch.embedding(self.w_emb, x) + self.w_pos[:, x.shape[-1]]
+        h_dec = h_0
+        for block in self.decoder_blocks:
+            h_dec = block(h_dec, pad_mask)
+        logits = h_dec @ self.w_emb.T
+        return F.softmax(logits, dim=-1)[:, -1]
